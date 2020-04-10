8246f825982db4d25fb9e3dae40e1d09ae7fc135
Tristan Chase <tristan.m.chase@gmail.com>
2020-04-10 09:47:20 -0400

    Add TODO section

--- binstall.sh
+++ binstall.sh
@@ -78,3 +78,16 @@ fi
 #-----------------------------------
 
 exit 0
+
+# TODO
+#
+# * Handle empty ${1} with ${1:-} and __usage()
+# * Update dependencies section
+# * Update usage, description, and options section
+# * Update function __cleanup()
+# * Check that _variable="variable definition" (make sure it's in quotes)
+# * Rename $variables to ${_variables}
+# * Rename function_name() to function __function_name()
+# * Modify command substitution to "$(this_style)"
+# * Clean up stray ;'s
+# * Insert script
