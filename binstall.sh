1e81d2ff3d0d69e170266c87e93daac8909189be
Tristan Chase <tristan.m.chase@gmail.com>
2020-04-16 15:15:19 -0400

    Imrpove functionality
    * If _arg_1 is empty, ask for filename else exit

--- binstall.sh
+++ binstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 set -o errtrace
-#set -x
+set -x
 IFS=$'\n\t'
 
 #-----------------------------------
@@ -52,6 +52,9 @@ function __cleanup() {
 		2) # exit 2; user termination
 			__info ""$(basename $0).$$": script terminated by user."
 			;;
+		3) # exit 2; user termination
+			__info ""$(basename $0).$$": ${FUNCNAME[1]} test: ok."
+			;;
 		*) # any other exit number; indicates an error in the script
 			#clean up stray files
 			#__fatal ""$(basename $0).$$": [error message here]"
@@ -68,13 +71,47 @@ if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
 	trap __cleanup EXIT
 #-----------------------------------
 # Main Script goes here
-# TODO Handle empty ${1} with ask for filename
-# TODO Handle if ${1}=filename.sh
+_name="${1:-}"
+
+# * If _arg_1 is empty, ask for filename else exit
+function __arg_1_empty(){
+		printf "Enter the name of the script you would like to install (blank quits): "
+		read _name
+		if [[ -z ${_name:-} ]]; then
+			exit 2
+		fi
+		__do_install
+}
+
+_devel_dir="${HOME}/devel/${_name}"
+_sh_file="${_devel_dir}/${_name}.sh"
+_bin_dir="${HOME}/bin"
+_bin_file="${_bin_dir}/${_name}"
+
+# TODO Handle if _arg_1=filename.sh
+	#__arg_1_has_dot_sh
+# TODO Handle arg _arg_1 does not exist
+	#__arg_1_not_exists
+
+function __do_install(){
+	rsync --update "${HOME}"/devel/"${_name:-}"/"${_name:-}".sh "${HOME}"/bin/"${_name:-}"
+	#rsync --update "${HOME}"/devel/"${1:-}"/"${1:-}".sh "${HOME}"/bin/"${1:-}"
+	
+}
+
+# Runtime
 if [[ -z "${1:-}" ]]; then
+	__arg_1_empty
+# elif Handle if _arg_1=filename.sh
+	#__arg_1_has_dot_sh
+# elif Handle arg _arg_1 does not exist
+	#__arg_1_not_exists
+elif [[ "${1:-}" =~ (-h|--help) ]]; then
 	__usage
 else
-	rsync --update "${HOME}"/devel/"${1:-}"/"${1:-}".sh "${HOME}"/bin/"${1:-}"
+	__do_install
 fi
+# End runtime
 
 # Main Script ends here
 #-----------------------------------
@@ -90,3 +127,4 @@ exit 0
 #
 # * Update dependencies section
 # * Update usage, description, and options section
+# + If _arg_1 is empty, ask for filename else exit
